0485886d8c9ef16c4f50f7211d7cd34ef62e0fb9
Manoel Brasil <manoelbo@hotmail.com>
2025-01-21 15:47:19 -0300

    Refactor Home component layout: update ESLint configuration, enhance .gitignore for local environment files, and improve structure of the main page for better styling and organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import {
   Heart,
   Star,
@@ -11,14 +10,16 @@ import {
   Users2,
 } from "lucide-react"
 
-export default function LandingPage() {
+export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      <WhatAreFanTruths />
-      <OurProcess />
-      <WhatYoullGet />
-      <CTASection />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <HeroSection />
+        <WhatAreFanTruths />
+        <OurProcess />
+        <WhatYoullGet />
+        <CTASection />
+      </div>
     </main>
   )
 }
